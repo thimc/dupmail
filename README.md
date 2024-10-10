65a48b2df719586b54a82237608d1d941c5cb11b
Thim Cederlund <thim@cederlund.de>
2024-10-10 22:00:30 +0200

    Remove all the unnecessary complexity and be generally less smart
    Determining if a mail file is a duplicate based on a string isn't
    hard, so stop trying to be so smart about it.
    
    The summary flag (`-s`) has been dropped in favour of `-n` which
    tells dupmail to not delete any duplicate files.

--- README.md
+++ README.md
@@ -1,25 +1,21 @@
 # dupmail
 
 dupmail scans a directory that (presumably) contains offline e-mail
-files. The program deems a mail a duplicate by comparing the Message-ID header
-and in case there isn't one the mail is simply skipped.
+files which are usually fetched by programs such as offlineimap,
+mbsync or fdm. The program deems a mail a duplicate by comparing
+the Message-ID header and in case there isn't one the mail is simply
+skipped.
 
-These mails are typically fetched by a program such as OfflineIMAP,
-mbsync or fdm and should work nicely with any of them.
-
-On exit dupmail will print the file paths of all the duplicates.
+On exit dupmail will print the message id and file paths of all the
+duplicates.
 
 ## Installation
 
-    $ go build -o dupmail dupmail.go
-    # cp dupmail /usr/local/bin/
+    # go build -o /usr/local/bin/dupmail main.go
 
 ## Usage
 
-`dupmail [-d maildir] [-v] [-n] [-s]`
+`dupmail [-d maildir] [-n]`
 
 * `-d`	maildir to be scanned
 * `-n`	scan the maildir but do not delete any files
-* `-s`	print a summary instead of the file paths of the duplicates
-* `-v`  increase verbosity. Dupmail will print out the duplicates
-in a very script friendly "\<Message-ID> \<TAB> \<File Path>" format.
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+var (
+	dirpath = flag.String("d", "", "maildir to be scanned")
+	noop    = flag.Bool("n", false, "scan the maildir but do not delete any files")
+
+	messageHeader = "message-id:"
+)
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage for %s\n", filepath.Base(os.Args[0]))
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	var (
+		mail        = make(map[string]bool)
+		start       = time.Now()
+		mails, dups int
+	)
+	if *dirpath == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	if _, err := os.Stat(*dirpath); err != nil {
+		fmt.Fprintf(os.Stderr, "stat %q: %s\n", *dirpath, err)
+		os.Exit(1)
+	}
+	if err := filepath.Walk(*dirpath, func(path string, info fs.FileInfo, err error) error {
+		if !info.Mode().IsRegular() {
+			return err
+		}
+		mails++
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		r := bufio.NewReader(f)
+		for {
+			ln, err := r.ReadString('\n')
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				return err
+			}
+			if !strings.HasPrefix(strings.ToLower(ln), messageHeader) {
+				continue
+			}
+			var (
+				parts = strings.Split(ln[:len(ln)-1], " ")
+				msgid = strings.Join(parts[1:], "")
+			)
+			if !mail[msgid] {
+				mail[msgid] = true
+				continue
+			}
+			fmt.Printf("%s\t%s\n", msgid, path)
+			if *noop {
+				continue
+			}
+			if err := os.Remove(path); err != nil {
+				return err
+			}
+			dups++
+			return nil
+		}
+		return nil
+	}); err != nil {
+		fmt.Fprintf(os.Stderr, "walk %q: %s\n", *dirpath, err)
+		os.Exit(1)
+	}
+	fmt.Printf("processed %d mails in %.2fs. %d duplicates found\n", mails, time.Since(start).Seconds(), dups)
+}
